13c08cd96138bf5a44c50102c3ef02d9b4c74a8e
samuelakena <akenasg@gmail.com>
2022-01-27 09:40:07 +0100

    initial commit

---
+++ src/RecomendedVideos.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './RecomendedVideos.css';
+import Video from './Video'
+
+function RecomendedVideos() {
+    return (
+        <div className='recomendedVideos'>
+            <h2>Recommended</h2>
+
+            <div className='recomendedVideos__videos'>
+                <Video 
+                image="https://media.istockphoto.com/photos/engaging-with-international-clients-picture-id1333358904?b=1&k=20&m=1333358904&s=170667a&w=0&h=EsLisOfP0SimgbBqU1s0X1gv1A31-pTWCu3KrjOVScg="
+                title="1st youtube video"
+                channel="kus"
+                views="100 views"
+                timestamp="1 day ago"                 
+                channelImage="https://images.unsplash.com/photo-1620415063111-5cff67c31e00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjcxfHxmYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <Video 
+                image="https://media.istockphoto.com/photos/engaging-with-international-clients-picture-id1333358904?b=1&k=20&m=1333358904&s=170667a&w=0&h=EsLisOfP0SimgbBqU1s0X1gv1A31-pTWCu3KrjOVScg="
+                title="1st youtube video"
+                channel="kus"
+                views="100 views"
+                timestamp="1 day ago"                 
+                channelImage="https://images.unsplash.com/photo-1620415063111-5cff67c31e00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjcxfHxmYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <Video 
+                image="https://media.istockphoto.com/photos/engaging-with-international-clients-picture-id1333358904?b=1&k=20&m=1333358904&s=170667a&w=0&h=EsLisOfP0SimgbBqU1s0X1gv1A31-pTWCu3KrjOVScg="
+                title="1st youtube video"
+                channel="kus"
+                views="100 views"
+                timestamp="1 day ago"                 
+                channelImage="https://images.unsplash.com/photo-1620415063111-5cff67c31e00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjcxfHxmYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <Video 
+                image="https://media.istockphoto.com/photos/engaging-with-international-clients-picture-id1333358904?b=1&k=20&m=1333358904&s=170667a&w=0&h=EsLisOfP0SimgbBqU1s0X1gv1A31-pTWCu3KrjOVScg="
+                title="1st youtube video"
+                channel="kus"
+                views="100 views"
+                timestamp="1 day ago"                 
+                channelImage="https://images.unsplash.com/photo-1620415063111-5cff67c31e00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjcxfHxmYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <Video 
+                image="https://media.istockphoto.com/photos/engaging-with-international-clients-picture-id1333358904?b=1&k=20&m=1333358904&s=170667a&w=0&h=EsLisOfP0SimgbBqU1s0X1gv1A31-pTWCu3KrjOVScg="
+                title="1st youtube video"
+                channel="kus"
+                views="100 views"
+                timestamp="1 day ago"                 
+                channelImage="https://images.unsplash.com/photo-1620415063111-5cff67c31e00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjcxfHxmYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <Video 
+                image="https://media.istockphoto.com/photos/engaging-with-international-clients-picture-id1333358904?b=1&k=20&m=1333358904&s=170667a&w=0&h=EsLisOfP0SimgbBqU1s0X1gv1A31-pTWCu3KrjOVScg="
+                title="1st youtube video"
+                channel="kus"
+                views="100 views"
+                timestamp="1 day ago"                 
+                channelImage="https://images.unsplash.com/photo-1620415063111-5cff67c31e00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjcxfHxmYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+            </div>
+            
+        </div>
+    )
+}
+
+export default RecomendedVideos
---
+++ src/Video.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Video.css';
+import Avatar from '@mui/material/Avatar';
+
+function Video({ image, title, channel, views, timestamp, channelImage }) {
+  return (
+    <div className="video">
+      <img className="video__thumbnail" src={image} alt="" />
+      <div className="video__info">
+        <Avatar className="video__avatar" alt={channel} src={channelImage} />
+        <div className="video__text">
+          <h4> {title} </h4>
+          <p>{channel}</p>
+          <p>
+            {views} . {timestamp}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Video;
